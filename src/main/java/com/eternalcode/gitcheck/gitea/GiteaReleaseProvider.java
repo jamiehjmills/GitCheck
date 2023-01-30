71185b659ca4334ef76f2e8dbfefad81c5c067c2
Hyojoo Mills <hyojoo.mills@gmail.com>
2023-01-30 19:48:46 +1100

    updated Http and GiteaaReleaseProvider

--- src/main/java/com/eternalcode/gitcheck/gitea/GiteaReleaseProvider.java
+++ src/main/java/com/eternalcode/gitcheck/gitea/GiteaReleaseProvider.java
@@ -3,15 +3,22 @@ package com.eternalcode.gitcheck.gitea;
 import com.eternalcode.gitcheck.git.GitRelease;
 import com.eternalcode.gitcheck.git.GitReleaseProvider;
 import com.eternalcode.gitcheck.git.GitRepository;
+import com.eternalcode.gitcheck.shared.Http;
+import org.json.simple.JSONObject;
 
 public class GiteaReleaseProvider implements GitReleaseProvider {
 
-
-    private static final String GET_LATEST_RELEASE = "https://try.gitea.io/api/v1/repos/%s/tags/%s";
-
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_LATEST_RELEASE = "https://try.gitea.io/api/v1/repos/%s/tags?limit=1";
 
     @Override
     public GitRelease getLatestRelease(GitRepository repository) {
+
+        Http http = new Http(repository, GET_LATEST_RELEASE, USER_AGENT);
+        JSONObject json = http.requestLastRelease();
+
+        //create my own giteaRelease builder
+
         return null;
     }
 }
